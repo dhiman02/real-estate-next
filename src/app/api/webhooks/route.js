c09b99122a5e6e327501f8dfe20530fe9bdcca94
Namrata Dhiman <n.dhiman.0016@gmail.com>
2025-01-30 03:50:05 +0530

    fixed a typo

--- src/app/api/webhooks/route.js
+++ src/app/api/webhooks/route.js
@@ -64,7 +64,7 @@ export async function POST(req) {
         );
         if(user && eventType === 'user.created'){
             try{
-                await clerkClient.user.createOrUpdateUserMetaData(id, {
+                await clerkClient.users.createOrUpdateUserMetadata(id, {
                     publicMetadata:{
                         userMongoId: user_id,
                     },
